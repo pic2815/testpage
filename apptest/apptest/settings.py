628bc8d59d9ca9cbbc49cb40ef8f9f3789777c43
pic2815 <77369807+pic2815@users.noreply.github.com>
2022-03-02 17:31:51 +0900

    Update settings.py

--- apptest/apptest/settings.py
+++ apptest/apptest/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'ec2-3-38-150-7.ap-northeast-2.compute.amazonaws.com'
-    'ELB-2054671012.ap-northeast-2.elb.amazonaws.com'
+    'ELB-417984140.ap-northeast-2.elb.amazonaws.com'
 ]
 
 
